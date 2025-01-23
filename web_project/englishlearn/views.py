205bedd1f3ee604e0c4238c0abd7d065a4f925a5
M0onKn1ght <jakub05k@vp.pl>
2025-01-23 14:37:58 +0100

    Update views.py

--- web_project/englishlearn/views.py
+++ web_project/englishlearn/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Language, Level
 
 def language_list(request):
@@ -15,4 +15,6 @@ def level_details(request, level_id):
     resources = level.learning_resources.split(',')
     return render(request, 'englishlearn/level_details.html', {'level': level, 'resources': resources})
 
+def home_view(request):
+    return render(request, 'home.html')
 # Create your views here.
