32d2b44c81c9b33655c533a6b8d5b3d23f34b674
TheDino13 <108119649+TheDino13@users.noreply.github.com>
2025-01-25 20:16:06 +0100

    deleted everything to redo

--- web_project/mysite/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home_view(request):
-    if request.user.is_authenticated:
-        print(f"User  logged in: {request.user.username}")
-    else:
-        print("User not logged in")
-    return render(request, 'home.html')
