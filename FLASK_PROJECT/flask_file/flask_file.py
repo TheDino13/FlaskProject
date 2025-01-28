1be1753673f28763976ebd4a0b70852836e92f20
parallel43w <prochelll9@gmail.com>
2025-01-28 14:42:41 +0100

    deleting the row

--- FLASK_PROJECT/flask_file/flask_file.py
+++ FLASK_PROJECT/flask_file/flask_file.py
@@ -15,7 +15,6 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
 # User Model
 class User(db.Model, UserMixin):
    id = db.Column(db.Integer, primary_key=True)
